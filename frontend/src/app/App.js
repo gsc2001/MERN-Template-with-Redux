9ac270a6ad48f6a157a3b4e5326d5346676971e8
Gurkirat Singh <gurkirat.singh@students.iiit.ac.in>
2021-01-14 18:27:51 +0530

    [Add] react router setup

--- frontend/src/app/App.js
+++ frontend/src/app/App.js
@@ -1,4 +1,7 @@
 import { Provider as StoreProvider } from 'react-redux';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Profile from '../components/Home';
+import Main from '../components/Main';
 import './App.css';
 
 import store from './store';
@@ -6,9 +9,12 @@ import store from './store';
 function App() {
     return (
         <StoreProvider store={store}>
-            <div className="App">
-                <h1>App!</h1>
-            </div>
+            <Router>
+                <Switch>
+                    <Route path="/" exact component={Main} />
+                    <Route path="/profile" exact component={Profile} />
+                </Switch>
+            </Router>
         </StoreProvider>
     );
 }
